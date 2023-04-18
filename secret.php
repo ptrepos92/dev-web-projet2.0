a7f972053b48bc742e3d14ff91a930ecc2852f45
ptrepos92 <104291849+ptrepos92@users.noreply.github.com>
2023-04-18 18:53:14 +0200

    Update secret.php

--- secret.php
+++ secret.php
@@ -1,9 +1,79 @@
-<?php 
+<!DOCTYPE  html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <style>
+        * {
+            font-family: arial;
+        }
+ 
+        body {
+            margin: 20px;
+        }
+ 
+        form {
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            margin-left: -150px;
+            margin-top: -100px;
+        }
+ 
+        h1 {
+            text-align: center;
+            color: #FFFAFA;
+            background: gray;
+        }
+ 
+        input[type=submit] {
+            border: solid 1px violet;
+            margin-bottom: 10px;
+            float: right;
+            padding: 15px;
+            outline: none;
+            border-radius: 7px;
+            width: 120px;
+        }
+ 
+        input {
+            border: solid 1px violet;
+            margin-bottom: 10px;
+            padding: 16px;
+            outline: none;
+            border-radius: 7px;
+            width: 300px;
+        }
+ 
+        .erreur {
+            text-align: center;
+            color: red;
+            margin-top: 10px;
+        }
+ 
+        a {
+            font-size: 14pt;
+            color: blue;
+            text-decoration: none;
+            font-weight: normal;
+        }
+ 
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
 
-if (!isset($_SESSION['username']))
-    header("Location:connexion.php");
-?>
-<?php include 'entete.html' ?>
+<body>
+<h1>Authentification réussi</h1>
 
-<h2>BLaaa</h2>
-<?php include 'footer.html' ?>
+
+<h2> Page Accueil</h2>
+
+
+
+<input type="text"  name="valider" value="S'authentifier">
+
+
+</body>
+</html>
+
